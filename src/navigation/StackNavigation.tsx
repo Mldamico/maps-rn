c6c06d68c984fac410619b1bc8d83505c134a804
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-24 12:16:15 -0300

    Basic setup

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,12 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {HomeScreen} from '../screens/HomeScreen';
+const Stack = createStackNavigator();
+
+export function StackNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
