e1c29776c6cc9226d2cfb8ba0658f213965c7d54
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-25 10:55:03 -0300

    Floating action button component

---
+++ src/components/Fab.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface Props {
+  iconName: string;
+  onPress: () => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const Fab = ({iconName, onPress, style = {}}: Props) => {
+  return (
+    <View style={{...(style as any)}}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={onPress}
+        style={styles.blackButton}>
+        <Icon name={iconName} size={35} color="white" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  blackButton: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'black',
+    zIndex: 9999,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+});
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import {useLocation} from '../hooks/useLocation';
 import {LoadingScreen} from '../screens/LoadingScreen';
+import {Fab} from './Fab';
 
 interface MapProps {}
 
@@ -13,23 +14,30 @@ export const Map: React.FC<MapProps> = ({}) => {
     return <LoadingScreen />;
   }
   return (
-    <MapView
-      style={{flex: 1}}
-      // provider={PROVIDER_GOOGLE}
-      showsUserLocation
-      initialRegion={{
-        latitude: initialPosition.latitude,
-        longitude: initialPosition.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}>
-      {/* <Marker
+    <>
+      <MapView
+        style={{flex: 1}}
+        // provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+          latitude: initialPosition.latitude,
+          longitude: initialPosition.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        {/* <Marker
         image={require('../assets/custom-marker.png')}
         coordinate={{latitude: 37.78825, longitude: -122.4324}}
         title="titulo"
         description="desc"
       /> */}
-    </MapView>
+      </MapView>
+      <Fab
+        iconName="star-outline"
+        onPress={() => {}}
+        style={{position: 'absolute', bottom: 10, right: 10}}
+      />
+    </>
   );
 };
 
